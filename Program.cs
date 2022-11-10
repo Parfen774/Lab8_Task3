f79e15311506c25d601b5c7298594cd2faab0258
Parfen774 <113260962+Parfen774@users.noreply.github.com>
2022-11-11 00:00:16 +0500

    Added pretty print

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace Progaram
         public static void Main()
         {
             string[] array = new string[] { "code", "doce", "ecod", "framer", "frame" };
-            List<string> result = new List<string>();
+            List<string> newArray = new List<string>();
             string lastWord = "";
 
             for (int i = 0; i < array.Length; i++)
@@ -17,14 +17,13 @@ namespace Progaram
 
                 if (word != lastWord)
                 {
-                    result.Add(array[i]);
+                    newArray.Add(array[i]);
                     lastWord = word;
                 }
             }
 
-            string[] res = result.ToArray();
-            foreach (string item in res)
-                Console.Write(item + " ");
+            string[] result = newArray.ToArray();
+            PrettyPrint(result);
         }
 
         public static string SortSymbol(string word)
@@ -33,6 +32,18 @@ namespace Progaram
             Array.Sort(charsArray);
 
             return String.Join("", charsArray);
+        }
+
+        public static void PrettyPrint(string[] array)
+        {
+            string result = "";
+
+            foreach (var item in array)
+            {
+                result += $"\'{item}\' ";
+            }
+
+            Console.WriteLine($"[{result.Trim()}]");
         }
     }
 }
