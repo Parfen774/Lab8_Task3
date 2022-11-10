32786fc82d56753856f37e47c6c6da34d750c0c3
Parfen774 <113260962+Parfen774@users.noreply.github.com>
2022-11-10 23:52:27 +0500

    Task Completed

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Progaram
 {
@@ -6,7 +7,32 @@ namespace Progaram
     {
         public static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            string[] array = new string[] { "code", "doce", "ecod", "framer", "frame" };
+            List<string> result = new List<string>();
+            string lastWord = "";
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                string word = SortSymbol(array[i]);
+
+                if (word != lastWord)
+                {
+                    result.Add(array[i]);
+                    lastWord = word;
+                }
+            }
+
+            string[] res = result.ToArray();
+            foreach (string item in res)
+                Console.Write(item + " ");
+        }
+
+        public static string SortSymbol(string word)
+        {
+            char[] charsArray = word.ToCharArray();
+            Array.Sort(charsArray);
+
+            return String.Join("", charsArray);
         }
     }
 }
